a21b7cd910206320a95123845034485c86b39ef3
tksander <tkepasander@gmail.com>
2015-07-24 19:20:17 -0700

    Initial programming for function - built out basic framework. Needs work.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,50 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
 
-// But instead we're going to implement it from scratch:
+// --------------------------------------------------
+//  getElementByClassName
+// --------------------------------------------------
 var getElementsByClassName = function(className
 ){
-  // your code here
+
+    var elementArr = [];
+    var body = document.body
+    
+    (function recurse(element) {
+
+        // current element contains the classname
+        if(element.classList.contains(className)) {
+            elementArr.push(element)
+        }
+
+        // if the element has childnodes
+        else if(element.hasChildNodes()) {
+            var children = element.childNodes
+            for(var i = 0; i < children.length; i++) {
+                recurse(children[i])
+            }
+        }
+    })(body);
+
+
+
+    // return the array populated with elements
+    return elementArr
 };
+
+
+/*
+Returns an array-like object of all
+child elements which have all of the
+given class names
+
+Dcoument model is searched, including
+the root node
+
+
+element.classList // returns a token list of 
+the class attribute of the element
+
+element.childNodes // returns a live colleciton 
+of child nodes of the given element
+
+
+*/
